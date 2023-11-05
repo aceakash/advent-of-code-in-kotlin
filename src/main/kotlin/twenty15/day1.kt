3568cbf4a2fbedb4942a52d3fb7b5e548bbfa9e8
Akash Kurdekar <akash.kurdekar@sedex.com>
2023-11-05 00:53:19 +0000

    part 2

--- src/main/kotlin/twenty15/day1.kt
+++ src/main/kotlin/twenty15/day1.kt
@@ -3,3 +3,17 @@ package twenty15
 fun partOne(input: String): Int {
     return input.fold(0) { next, acc -> next + if (acc == '(') 1 else -1 }
 }
+
+fun partTwo(input: String): Int {
+    var floor = 0
+    for (i in input.indices) {
+        when (input[i]) {
+            '(' -> floor++
+            ')' -> floor--
+        }
+        if (floor == -1) {
+            return i+1
+        }
+    }
+    return 0
+}
