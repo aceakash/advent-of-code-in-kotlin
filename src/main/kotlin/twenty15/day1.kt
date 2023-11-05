30885434b7cdc698b85fb5db2bddd62969a13f04
Akash Kurdekar <akash.kurdekar@sedex.com>
2023-11-05 01:01:44 +0000

    refactor

--- src/main/kotlin/twenty15/day1.kt
+++ src/main/kotlin/twenty15/day1.kt
@@ -1,18 +1,23 @@
 package twenty15
 
+val move = { c: Char ->
+    when (c) {
+        '(' -> 1
+        ')' -> -1
+        else -> 0
+    }
+}
+
 fun partOne(input: String): Int {
-    return input.fold(0) { next, acc -> next + if (acc == '(') 1 else -1 }
+    return input.fold(0) { next, acc -> next + move(acc) }
 }
 
 fun partTwo(input: String): Int {
     var floor = 0
     for (i in input.indices) {
-        when (input[i]) {
-            '(' -> floor++
-            ')' -> floor--
-        }
+        floor += move(input[i])
         if (floor == -1) {
-            return i+1
+            return i + 1
         }
     }
     return 0
