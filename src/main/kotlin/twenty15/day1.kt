5dcfe2bdeeb7cd9d3610f91ec987529d6b55bd1b
Akash Kurdekar <akash.kurdekar@sedex.com>
2023-11-27 17:52:57 +0000

    day2 part 1

--- src/main/kotlin/twenty15/day1.kt
+++ src/main/kotlin/twenty15/day1.kt
@@ -1,24 +1,30 @@
 package twenty15
 
-val move = { c: Char ->
-    when (c) {
-        '(' -> 1
-        ')' -> -1
-        else -> 0
+
+
+
+
+class Day1 {
+    fun partOne(input: String): Int {
+        return input.fold(0) { next, acc -> next + move(acc) }
     }
-}
 
-fun partOne(input: String): Int {
-    return input.fold(0) { next, acc -> next + move(acc) }
-}
+    fun partTwo(input: String): Int {
+        var floor = 0
+        for (i in input.indices) {
+            floor += move(input[i])
+            if (floor == -1) {
+                return i + 1
+            }
+        }
+        return 0
+    }
 
-fun partTwo(input: String): Int {
-    var floor = 0
-    for (i in input.indices) {
-        floor += move(input[i])
-        if (floor == -1) {
-            return i + 1
+    val move = { c: Char ->
+        when (c) {
+            '(' -> 1
+            ')' -> -1
+            else -> 0
         }
     }
-    return 0
 }
---
+++ src/test/kotlin/twenty15/Day2.kt
@@ -0,0 +1,21 @@
+package twenty15
+
+
+class Day2 {
+
+    fun partOne(rawInput: String): Int {
+        return parseInput(rawInput).sumOf { present ->
+            val dims = present.sorted()
+            val slack = dims[0] * dims[1]
+            (2 * dims[0] * dims[1]) + (2 * dims[0] * dims[2]) + (2 * dims[2] * dims[1]) + slack
+        }
+    }
+
+
+    private fun parseInput(rawInput: String): List<List<Int>> {
+        return rawInput.split('\n')
+            .map { line: String ->
+                line.split('x').map { it.toInt() }
+            }
+    }
+}
