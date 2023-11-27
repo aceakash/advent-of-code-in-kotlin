fd4c3d3eba4d2ae72052e721c1938002d9d655d4
Akash Kurdekar <akash.kurdekar@sedex.com>
2023-11-27 18:26:04 +0000

    day3 part 2 - first cut

--- src/test/kotlin/twenty15/Day3.kt
+++ src/test/kotlin/twenty15/Day3.kt
@@ -10,10 +10,14 @@ class Day3 {
     fun partOne(rawInput: String): Int {
         val parsed = parseInput(rawInput)
 
+        return visit(parsed).size
+    }
+
+    fun visit(directions: List<Direction>): Set<Pair<Int, Int>> {
         var curr = Pair(0, 0)
         val visited = mutableSetOf(curr) // starting location
-        parsed.forEach { direction ->
-            val newLocation = when(direction) {
+        directions.forEach { direction ->
+            val newLocation = when (direction) {
                 Direction.N -> Pair(curr.first, curr.second + 1)
                 Direction.S -> Pair(curr.first, curr.second - 1)
                 Direction.E -> Pair(curr.first + 1, curr.second)
@@ -24,11 +28,22 @@ class Day3 {
             curr = newLocation
         }
 
-        return visited.size
+        return visited
     }
 
     fun partTwo(rawInput: String): Int {
-        return 42
+        val parsed = parseInput(rawInput)
+
+        val evens = parsed.filterIndexed { index, _ -> index % 2 == 0 }
+        val odds = parsed.filterIndexed { index, _ -> index % 2 == 1 }
+
+        val set1 = setOf(Pair(0, 0))
+        val set2 = visit(evens)
+        val set3 = visit(odds)
+
+        return (set1 union set2 union set3).size
+
+
     }
 
 
