b84a19e5bffdb4e58b758eac668dd6d45da708c5
Akash Kurdekar <akash.kurdekar@sedex.com>
2023-12-02 12:09:34 +0000

    more

--- src/main/kotlin/twenty23/Day2.kt
+++ src/main/kotlin/twenty23/Day2.kt
@@ -2,34 +2,34 @@ package twenty23
 
 class Day2 {
     fun part1(input: String): Int {
-        val games = input.split("\n")
-            .mapIndexed { i, l -> Game.fromString(i, l) }
-
         val maxCountsByColour = mapOf("red" to 12, "green" to 13, "blue" to 14)
 
-        return games.filter { it.isPossible(maxCountsByColour) }
+        return Game.parseMultipleGames(input)
+            .filter { it.isPossible(maxCountsByColour) }
             .sumOf { it.gameNum }
     }
 
     fun part2(input: String): Int {
-        val games = input.split("\n")
-            .mapIndexed { i, l -> Game.fromString(i, l) }
-
-        return games.sumOf {
-            val maxByColour = it.getMaxRGB()
-            (maxByColour.first * maxByColour.second * maxByColour.third)
-        }
+        return Game.parseMultipleGames(input)
+            .sumOf {
+                val maxByColour = it.getMaxRGB()
+                (maxByColour.first * maxByColour.second * maxByColour.third)
+            }
     }
 
     private data class Game(val gameNum: Int = 0, val sets: List<GameSet> = emptyList()) {
         companion object {
-            fun fromString(index: Int, str: String): Game {
+            fun parseSingleGame(index: Int, str: String): Game {
                 val setInfo = str.split(": ")[1]
                 val gameSets = setInfo.split("; ").map {
                     GameSet.fromString(it)
                 }
-                //        println("gameSets: $gameSets")
                 return Game(index + 1, gameSets)
+            }
+
+            fun parseMultipleGames(str: String): List<Game> {
+                return str.split("\n")
+                    .mapIndexed { i, l -> parseSingleGame(i, l) }
             }
         }
 
@@ -44,6 +44,7 @@ class Day2 {
                 sets.maxBy { set -> set.blue }.blue
             )
         }
+
         private data class GameSet(
             val red: Int = 0,
             val green: Int = 0,
@@ -55,29 +56,16 @@ class Day2 {
                         && this.blue <= (maxCountsByColour["blue"] ?: 0))
             }
 
-//            private fun parseGameSetFromString(gameSetStr: String): GameSet {
-////        println("gameSetStr: $gameSetStr")
-//                val gameSetMap = mutableMapOf<String, Int>()
-//                gameSetStr.split(", ").forEach {
-//                    // x colour
-//                    val parts = it.split(" ")
-//                    gameSetMap[parts[1]] = parts[0].toInt()
-//                }
-//                return GameSet(gameSetMap["red"] ?: 0, gameSetMap["green"] ?: 0, gameSetMap["blue"] ?: 0)
-//            }
-
             companion object {
                 fun fromString(gameSetStr: String): GameSet {
                     val gameSetMap = mutableMapOf<String, Int>()
                     gameSetStr.split(", ").forEach {
-                        // x colour
                         val parts = it.split(" ")
                         gameSetMap[parts[1]] = parts[0].toInt()
                     }
                     return GameSet(gameSetMap["red"] ?: 0, gameSetMap["green"] ?: 0, gameSetMap["blue"] ?: 0)
                 }
             }
-
         }
     }
 }
