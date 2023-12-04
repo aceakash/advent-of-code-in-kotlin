037cd45134edb9d73eb8101190a05d1799e6333a
Akash Kurdekar <akash.kurdekar@sedex.com>
2023-12-04 09:18:55 +0000

    day4 part 2

--- src/main/kotlin/twenty23/Day4.kt
+++ src/main/kotlin/twenty23/Day4.kt
@@ -2,26 +2,34 @@ package twenty23
 
 import kotlin.math.pow
 
+data class Card(val winningNums: List<Int>, val nums: List<Int>, var count: Int = 1)
 
 class Day4 {
     fun part1(input: String): Int {
-        val parsed: List<Pair<List<Int>, List<Int>>> = parseInput(input)
+        val parsed: List<Card> = parseInput(input)
 
-        return parsed.map { card ->
-            val matches = card.first.toSet().intersect(card.second.toSet())
-            if (matches.isEmpty()){
-                0
-            } else {
-                (2.0).pow(matches.size - 1.0).toInt()
-            }
-        }.sum()
-
+        return parsed.sumOf { card ->
+            val matches = card.winningNums.toSet().intersect(card.nums.toSet())
+            if (matches.isEmpty()) 0 else (2.0).pow(matches.size - 1.0).toInt()
+        }
     }
 
-    private fun parseInput(input: String): List<Pair<List<Int>, List<Int>>> {
+    fun part2(input: String): Int {
+        val cards = parseInput(input)
+
+        cards.forEachIndexed { i, card ->
+            val matchCount = card.winningNums.toSet().intersect(card.nums.toSet()).size
+            for (j in 1..matchCount) {
+                cards[i + j].count += card.count
+            }
+        }
+        return cards.sumOf { it.count }
+    }
+
+    private fun parseInput(input: String): List<Card> {
         return input.split("\n").map { line ->
             val parts = line.split(":")[1].split("|")
-            Pair(parseNumList(parts[0]), parseNumList(parts[1]))
+            Card(parseNumList(parts[0]), parseNumList(parts[1]))
         }
     }
 
@@ -30,8 +38,4 @@ class Day4 {
             it.trim().toInt()
         }
     }
-
-
-
-
 }
