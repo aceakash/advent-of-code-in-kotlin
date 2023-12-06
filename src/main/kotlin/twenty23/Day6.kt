cad3d3f602741818d9dc4cc4abc4939ca5ad2966
Akash Kurdekar <akash.kurdekar@sedex.com>
2023-12-06 15:14:56 +0000

    day6

--- src/main/kotlin/twenty23/Day6.kt
+++ src/main/kotlin/twenty23/Day6.kt
@@ -1,21 +1,21 @@
 package twenty23
 
 class Day6 {
-    fun part1(input: List<Pair<Int, Int>>): Int {
+    fun part1(input: List<Pair<Long, Long>>): Long {
         return input.map { numberOfWaysToWin(it) }
             .fold(1) { acc, count ->
                 acc * count
             }
     }
 
-    private fun numberOfWaysToWin(timeAndDistance: Pair<Int, Int>): Int {
+    private fun numberOfWaysToWin(timeAndDistance: Pair<Long, Long>): Int {
         val (time, distance) = timeAndDistance
-        return (0..time).count { hold ->
+        return (1 until time).count { hold ->
             hold * (time - hold) > distance
         }
     }
 
-    fun part2(input: String): Int {
-        return 42
+    fun part2(input: List<Pair<Long, Long>>): Long {
+        return part1(input)
     }
 }
--- src/test/kotlin/twenty23/Day6Test.kt
+++ src/test/kotlin/twenty23/Day6Test.kt
@@ -8,28 +8,43 @@ class Day6Test {
     @Test
     fun `part1 - sample input`() {
         val input = listOf(
-            7 to 9,
-            15 to 40,
-            30 to 200,
+            7L to 9L,
+            15L to 40L,
+            30L to 200L,
         )
         assertEquals(288, Day6().part1(input))
     }
 
     @Test
     fun `part 1 - real input`() {
-        assertEquals(140220, Day6().part1(realInput()))
+        assertEquals(140220, Day6().part1(realInput1()))
     }
 
+    @Test
+    fun `part2 - sample input`() {
+        val input = listOf(
+            71530L to 940200L,
+        )
+        assertEquals(71503, Day6().part2(input))
+    }
 
+    @Test
+    fun `part 2 - real input`() {
+        assertEquals(39570185, Day6().part2(realInput2()))
+    }
 
-
-
-    private fun realInput(): List<Pair<Int, Int>> {
+    private fun realInput1(): List<Pair<Long, Long>> {
         return listOf(
-            53 to 333,
-            83 to 1635,
-            72 to 1289,
-            88 to 1532
+            53L to 333L,
+            83L to 1635L,
+            72L to 1289L,
+            88L to 1532L
+        )
+    }
+
+    private fun realInput2(): List<Pair<Long, Long>> {
+        return listOf(
+            53837288L to 333163512891532L,
         )
     }
 }
